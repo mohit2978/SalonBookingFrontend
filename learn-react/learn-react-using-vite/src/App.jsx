02a6e2d8092db51298955a48026382880a151ec3
mohit2978 <mohitrakheja007@gmail.com>
2025-03-18 23:12:45 +0530

    Props and map

--- learn-react/learn-react-using-vite/src/App.jsx
+++ learn-react/learn-react-using-vite/src/App.jsx
@@ -3,18 +3,34 @@ import './App.css'
 import MovieCard from "./component/MovieCard.jsx";
 import MovieList from "./component/MovieList.jsx";
 
-function MyButton() {
-    return (
-        <button>
-            I'm a button
-        </button>
-    );
-}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const movies = [
+        {
+            name: "Inception",
+            imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQovCe0H45fWwAtV31ajOdXRPTxSsMQgPIQ3lcZX_mAW0jXV3kH"
+        },
+        {
+            name: "The Dark Knight",
+            imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQkUywIUXDjHSQJIaNHYVs08osgBpF5Ot-xmB_omyEZeeRP9Xug"
+        },
+        {
+            name: "Interstellar",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9oW0XQlu1lo1G_49M-YwGzKR6rUg-CtflZj07HfbT8d2GwKWg"
+        },
+        {
+            name: "Pushpa 2",
+            imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTK_Cq35EB_KZmwZ-8M--dRvv-e6WPhQlaNFnoMXKXivM6AihMq"
+        },
+        {
+            name: "Welcome Back",
+            imageUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQaKPMJfQpfF5yYfdwnhzi87weKItq6ySZ2gBBuUAteOGHadaaK"
+        }
+    ];
 
-  return (
+
+    return (
     <>
       <div>
 
@@ -23,18 +39,11 @@ function App() {
           </h1>
 
       </div>
-        <div className="flex flex-wrap">
-            <MovieCard/>
-            <MovieCard/>
+        <div className="flex flex-wrap ">
+            {movies.map(movie => (
+                <MovieCard name={movie.name} imageURl={movie.imageUrl} />
+            ))}
         </div>
-        <MovieList/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-      </div>
-        <MyButton />
 
     </>
   )
--- learn-react/learn-react-using-vite/src/component/MovieCard.jsx
+++ learn-react/learn-react-using-vite/src/component/MovieCard.jsx
@@ -1,12 +1,18 @@
+import {useState} from "react";
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+    const [count, setCount] = useState(0)
     return (
         <div className="card border-4 border-dark">
-            <h1 className="text-amber-500 backdrop-blur-2xl">Pushpa2</h1>
-            <img className="w-96 rounded-3xl"
-                src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTK_Cq35EB_KZmwZ-8M--dRvv-e6WPhQlaNFnoMXKXivM6AihMq"
-                alt="Mohit Kumar" />
-            <button className="rounded-br-4xl font-sans text-violet-400 backdrop-contrast-125">Book Ticket</button>
+            <h1 className="text-amber-500 backdrop-blur-2xl">{props.name}</h1>
+            <img className="w-60 max-h-90  rounded-3xl"
+                src={props.imageURl}
+                alt="Movie" />
+            <button onClick={() => setCount((count) => count + 1)}
+                className="rounded-br-4xl font-sans text-violet-400 backdrop-contrast-125">Book Ticket</button>
+            <button onClick={() => setCount((count) => count - 1)}
+                    className="rounded-br-4xl font-sans text-violet-400 backdrop-contrast-125">Cancel Ticket</button>
+            <h5>Tickets booked count : {count}</h5>
         </div>
     )
 }
