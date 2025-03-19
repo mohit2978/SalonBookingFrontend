fc3672170a39e45f60acc233e5bbdf4ae9f8ab5f
mohit2978 <mohitrakheja007@gmail.com>
2025-03-19 16:29:07 +0530

    Navigation added

--- learn-react/learn-react-using-vite/src/component/MovieList.jsx
+++ learn-react/learn-react-using-vite/src/component/MovieList.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import MovieCard from "./MovieCard.jsx";
 
 const MovieList = () => {
+    const movies = [
+        {
+            name: "Inception",
+            imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQovCe0H45fWwAtV31ajOdXRPTxSsMQgPIQ3lcZX_mAW0jXV3kH"
+        },
+        {
+            name: "The Dark Knight",
+            imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQkUywIUXDjHSQJIaNHYVs08osgBpF5Ot-xmB_omyEZeeRP9Xug"
+        },
+        {
+            name: "Interstellar",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9oW0XQlu1lo1G_49M-YwGzKR6rUg-CtflZj07HfbT8d2GwKWg"
+        },
+        {
+            name: "Pushpa 2",
+            imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTK_Cq35EB_KZmwZ-8M--dRvv-e6WPhQlaNFnoMXKXivM6AihMq"
+        },
+        {
+            name: "Welcome Back",
+            imageUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQaKPMJfQpfF5yYfdwnhzi87weKItq6ySZ2gBBuUAteOGHadaaK"
+        }
+    ];
+
     return (
         <div>
-            <ul>
-                <li className="list-unstyled text-violet-400">Mohit Kumar</li>
-                <li className="list-unstyled text-amber-300">Devanshu</li>
-                <li className="list-unstyled text-amber-500">Devanshu Jain</li>
-            </ul>
+        <div>
+            <h1 className="text-9xl text-fuchsia-600 bg-amber-300 font-light underline ">
+                Neelam Cinema
+            </h1>
+
+        </div>
+                <div className="flex flex-wrap ">
+                    {movies.map(movie => (
+                        <MovieCard name={movie.name} imageURl={movie.imageUrl} />
+                    ))}
+                </div>
         </div>
     );
 };
---
+++ learn-react/learn-react-using-vite/src/component/NavBar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from "../Login/login.jsx";
+import {useNavigate} from "react-router-dom";
+
+function Navbar() {
+    const navigate = useNavigate();
+    return (
+        <div  className='flex justify-between px-20 py-4 border-b '>
+            <h1 onClick={()=>navigate("/list")} className='cursor-pointer'> Cinema </h1>
+             <button type='button' onClick={()=>navigate("/login")}>Login</button>
+        </div>
+    );
+}
+
+export default Navbar;
