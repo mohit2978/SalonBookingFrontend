3bcc09a9b6bcc427b2c613c9936c971ba7772147
mohit2978 <mohitrakheja007@gmail.com>
2025-03-24 21:21:04 +0530

    use Effect Hook

--- learn-react/learn-react-using-vite/src/App.jsx
+++ learn-react/learn-react-using-vite/src/App.jsx
@@ -4,19 +4,23 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./component/Login/login.jsx";
 import MovieList from "./component/MovieList.jsx";
 import Navbar from "./component/NavBar/navbar.jsx";
+import {useEffect} from "react";
 
 
 
 
 function App() {
-
-
+    useEffect(() => {
+        console.log("Inside use effect");
+    })
+    console.log("first line");
+    console.log("second line");
     return (
     <>
         < Navbar />
         <Routes>
             <Route path="/list" element={<MovieList/>} />
-            <Route path="/login" element={<Login/>} />
+            <Route path="/" element={<Login/>} />
         </Routes>
 
     </>
--- learn-react/learn-react-using-vite/src/component/NavBar/navbar.jsx
+++ learn-react/learn-react-using-vite/src/component/NavBar/navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     return (
         <div  className='flex justify-between px-20 py-4 border-b '>
             <h1 onClick={()=>navigate("/list")} className='cursor-pointer'> Cinema </h1>
-             <button type='button' onClick={()=>navigate("/login")}>Login</button>
+             <button type='button' onClick={()=>navigate("/")}>Login</button>
         </div>
     );
 }
